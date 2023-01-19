fb9967e6bb6ab282b499c0af95011a9f5eb64c54
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-19 12:12:25 +0300

    Person

--- src/main.java
+++ src/main.java
@@ -1,19 +1,43 @@
 public class main {
     public static void main(String[] args) {
-        Box box = new Box();
-        box.length = 10;
-        box.height = 10;
-        box.width = 10;
-        double volume = box.height* box.width* box.length;
+//        Box box = new Box();
+//        box.length = 10;
+//        box.height = 10;
+//        box.width = 10;
+//        double volume = box.height* box.width* box.length;
+//
+//        Box box1=new Box();
+//        box1.width=20;
+//        box1.height=20;
+//        box1.length=20;
+//
+//
+//        System.out.println(volume);
+//        System.out.println(box1.height* box1.length* box1.width);
 
-        Box box1=new Box();
-        box1.width=20;
-        box1.height=20;
-        box1.length=20;
+        Person human1= new Person();
+        human1.name=" Александр ";
+        human1.age=44;
+        human1.weight=82;
+
+        Person human2=new Person();
+        human2.name="Veronika";
+        human2.age=39;
+        human2.weight=71.100;
+
+        Person human3= new Person();
+        human3.name="Andreea";
+        human3.age=15;
+        human3.weight=65.2;
+
+        Person human4=new Person();
+        human4.name="Alexandru";
+        human4.age=4;
+        human4.weight=25.20;
+
+        System.out.println((human4.age+human3.age+human2.age+ human1.age)/4f);
 
 
-        System.out.println(volume);
-        System.out.println(box1.height* box1.length* box1.width);
 
 
     }
