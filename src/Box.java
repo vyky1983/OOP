fc943c85273656530d66a0ecacff3932f630d5d4
vyky1983 <veronica.cernenchii@mail.ru>
2023-01-26 22:06:11 +0300

    modified file Box

--- src/Box.java
+++ src/Box.java
@@ -3,18 +3,19 @@ public class Box {
     double width;
     double height;
 
-    Box(){
+    Box() {
         this(10);
     }
+
     Box(double length, double width, double height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
-    Box(double size){
+
+    Box(double size) {
         this(size, size, size);
     }
-
 
 
     void setDim(double length, double height, double width) {
@@ -23,6 +24,18 @@ public class Box {
         this.width = width;
     }
 
+    void compare(Box another) {
+        double currentVolume = getVolume();
+        double anotherVolume = another.getVolume();
+        if (currentVolume > anotherVolume) {
+            System.out.println("Current > Another");
+        } else if (currentVolume < anotherVolume) {
+            System.out.println("Current < Another");
+        } else {
+            System.out.println("Current == Another");
+
+        }
+    }
 
     double getVolume() {
         return length * width * height;
